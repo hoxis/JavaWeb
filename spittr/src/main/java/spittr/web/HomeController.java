6de2119dddd1665fb16b1e5f8ad199f968713bf7
hoxis <liuhao1960@gmail.com>
2016-01-21 13:40:12 +0800

    修改HomeController为类层级的控制器

--- spittr/src/main/java/spittr/web/HomeController.java
+++ spittr/src/main/java/spittr/web/HomeController.java
@@ -5,11 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller // 声明一个控制器
+@RequestMapping("/") // 控制器匹配路径
 public class HomeController {
 
-	@RequestMapping(value = "/", method = RequestMethod.GET) // 处理GET请求
+	@RequestMapping(method = RequestMethod.GET) // 处理GET请求
 	public String home() {
-		return "home";
+		return "home";// 视图名称
 	}
 
 }
