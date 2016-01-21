c25ec80aef9f5d6a900423568aa40d3ece4c3f0f
hoxis <liuhao1960@gmail.com>
2016-01-21 13:05:07 +0800

    HomeController测试类

--- spittr/src/test/java/spittr/web/HomeControllerTest.java
+++ spittr/src/test/java/spittr/web/HomeControllerTest.java
@@ -1,13 +1,19 @@
 package spittr.web;
 
 import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 public class HomeControllerTest {
 
 	@Test
-	public void testHomePage() {
+	public void testHomePage() throws Exception {
 		HomeController controller = new HomeController();
-		System.out.println(controller.home());
+		// 设置MockMvc
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(MockMvcResultMatchers.view().name("home"));
 	}
 
 }
