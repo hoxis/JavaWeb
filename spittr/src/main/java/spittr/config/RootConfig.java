c37c2c6141978ad9f4511ec3936923b7ef5f7b62
hoxis <liuhao1960@gmail.com>
2016-02-04 16:27:27 +0800

    忘了一个/，害我搞了很久

--- spittr/src/main/java/spittr/config/RootConfig.java
+++ spittr/src/main/java/spittr/config/RootConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan(basePackages = { "spitter" }, excludeFilters = {
+@ComponentScan(basePackages = { "spittr" }, excludeFilters = {
 		@Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class) })
 public class RootConfig {
 
--- spittr/src/main/java/spittr/config/WebConfig.java
+++ spittr/src/main/java/spittr/config/WebConfig.java
@@ -11,14 +11,14 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan("spitter.web") // 激活Spring MVC
+@ComponentScan("spittr.web") // 激活Spring MVC
 public class WebConfig extends WebMvcConfigurerAdapter {
 
 	// 配置一个JSP视图解析器
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB_INF/views");
+		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
 		resolver.setExposeContextBeansAsAttributes(true);
 		return resolver;
